9b5ecf2fb61cfdf5528fe7f62c8af68b28bea4ca
Nguyen Van Si <vansi18042001@gmail.com>
2022-02-24 15:40:21 +0700

    add project

---
+++ Buoi2/ListBooks/Controllers/BooksController.cs
@@ -0,0 +1,125 @@
+﻿using ListBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListBooks.Controllers
+{
+    public class BooksController : Controller
+    {
+        private List<BookModels> listBooks;
+
+        public BooksController()
+        {
+            listBooks = new List<BookModels>();
+            listBooks.Add(new BookModels()
+            {
+                Id = 1,
+                Title = "Dạy con làm giàu 1",
+                Author = "Robert T.Kiyosaki",
+                PublicYear = 2001,
+                Price = 100000,
+                Cover = "Content/images/dayconlamgiau1.jpg"
+            });
+            listBooks.Add(new BookModels()
+            {
+                Id = 2,
+                Title = "Dạy con làm giàu 2",
+                Author = "Robert T.Kiyosaki",
+                PublicYear = 2011,
+                Price = 200000,
+                Cover = "Content/images/dayconlamgiau2.jpg"
+            });
+            listBooks.Add(new BookModels()
+            {
+                Id = 3,
+                Title = "Dạy con làm giàu 3",
+                Author = "Robert T.Kiyosaki",
+                PublicYear = 2021,
+                Price = 300000,
+                Cover = "Content/images/dayconlamgiau3.jpg"
+            });
+            listBooks.Add(new BookModels()
+            {
+                Id = 4,
+                Title = "Dạy con làm giàu 4",
+                Author = "Robert T.Kiyosaki",
+                PublicYear = 2021,
+                Price = 400000,
+                Cover = "Content/images/dayconlamgiau4.jpg"
+            });
+            listBooks.Add(new BookModels()
+            {
+                Id = 5,
+                Title = "Dạy con làm giàu 5",
+                Author = "Robert T.Kiyosaki",
+                PublicYear = 2021,
+                Price = 500000,
+                Cover = "Content/images/dayconlamgiau5.jpg"
+            });
+        }
+
+        public ActionResult ListBooks()
+        {
+            ViewBag.TitlePageName = "Book view page";
+            return View(listBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            BookModels book = listBooks.Find(f => f.Id == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            BookModels book = listBooks.Find(b => b.Id == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BookModels book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var editBook = listBooks.Find(b => b.Id == book.Id);
+                    editBook.Title = book.Title;
+                    editBook.Author = book.Author;
+                    editBook.Cover = book.Cover;
+                    editBook.PublicYear = book.PublicYear;
+                    editBook.Price = book.Price;
+                    return View("ListBooks", listBooks);
+                }
+                catch (Exception)
+                {
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Input model not valide!");
+                return View(book);
+            }
+        }
+    }
+}
---
+++ Buoi2/ListBooks/Models/BookModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ListBooks.Models
+{
+    public class BookModels
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please input Title")]     
+        public string Title { get; set; }  
+        [Required(ErrorMessage ="Please input Author")]
+        [StringLength(50,ErrorMessage ="Author less than 50 characters")]
+        public string Author { get; set; }  
+        public  int PublicYear { get; set; }    
+        public  double Price { get; set; }  
+        public string Cover { get; set; }
+
+        public BookModels()
+        {
+        }
+
+        public BookModels(int id, string title, string author, int publicYear, double price, string cover)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            PublicYear = publicYear;
+            Price = price;
+            Cover = cover;
+        }
+    }
+}
